c780b5cc211531a695ea45a573e559171b5a5c7a
Sal Gerace <sgerace@gmail.com>
2014-07-02 17:23:49 -0400

    Added spec console output and proper options.

--- test/reporter.spec.js
+++ test/reporter.spec.js
@@ -1,1 +1,59 @@
 var Reporter = require('../lib/reporter');
+
+describe("Reporter", function() {
+
+    var reporter, on, expected;
+
+    beforeEach(function() {
+        runner = {
+            on: function(type, listener) {}
+        };
+        reporter = new Reporter(runner);
+        console.log(reporter);
+    });
+
+    it("should create a key for the suite", function() {
+
+        
+
+
+        
+    });
+
+    // it("should return the value FAILED for failing test", function() {
+    //     reporter.onTestEnd({
+    //         title: 'a description',
+    //         state: 'failed',
+    //         parent: {
+    //             title: 'a suite'
+    //         }
+    //     });
+
+    //     expected = {
+    //         "a suite" : {
+    //             "a description" : "FAILED"
+    //         }
+    //     }
+    //     assert.deepEqual(reporter.output, expected);
+    // });
+
+    // it("should flatten nested tests that have multiple suites", function() {
+    //     reporter.onTestEnd({
+    //         title: 'a description',
+    //         state: 'passed',
+    //         parent: {
+    //             title: 'a suite',
+    //             parent: {
+    //                 title: 'parent'
+    //             }
+    //         }
+    //     });
+    //     expected = {
+    //         "a suite parent" : {
+    //             "a description" : "PASSED"
+    //         }
+    //     }
+    //     assert.deepEqual(reporter.output, expected);
+    // });
+
+});
